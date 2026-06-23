8238256b9989180bdceba2196e487f383d31d0c2
Isiphile Simelane <cphilep12@gmail.com>
2026-06-23 02:45:05 +0200

    Delete api directory

--- api/admin-router.ts
+++
@@ -1,8 +0,0 @@
-import { createRouter, adminQuery } from "./middleware";
-import { getAdminStats } from "./queries/admin";
-
-export const adminRouter = createRouter({
-  getStats: adminQuery.query(async () => {
-    return getAdminStats();
-  }),
-});
